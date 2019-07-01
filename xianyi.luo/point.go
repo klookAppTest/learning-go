2a053b60e229f210ae53ef12c317a6042585ff58
xianyiLuo <klookuser@xianyi.local>
2019-07-01 10:00:16 +0800

    update

--- xianyi.luo/point.go
+++ xianyi.luo/point.go
@@ -9,13 +9,13 @@ func main(){
 	//var ptr2 *int
 	//ptr1 = &a
 	//ptr2 = &b
-	fmt.Println("传参之前a的值：%x,b的值：%x",a,b)
+	fmt.Printf("传参之前a的值：%d,b的值：%d\n",a,b)
 	// &代表传入参数的地址
 	c, d := simpleSwap(a,b)
-	fmt.Println("不用指针传入参数，a的值:%x ,b的值:%x",c,d)
+	fmt.Printf("不用指针传入参数，a的值:%d ,b的值:%d\n",c,d)
 	swap(&a,&b)
 
-	fmt.Println("传参之后a的值：%x,b的值：%x",a,b)
+	fmt.Printf("传参之后a的值：%d,b的值：%d\n",a,b)
 }
 // 交换变量的数据，传入指针则会改变参数的数据
 func swap(a *int, b *int)  {
